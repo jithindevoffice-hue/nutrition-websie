27cb58b62dd485ebfb071622f900f066373d53c7
jithusonic <jithusonic33@gmail.com>
2026-05-13 23:56:43 +0530

    refactor: remove unused Link import from AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,7 +2,6 @@ import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
 import { Utensils, Flower2, Brain } from 'lucide-react';
 import styles from './AboutPage.module.css';
-import { Link } from 'react-router-dom';
 
 import aboutHeroImg from '../assets/about-hero.jpg';
 import profilePic from '../assets/sonikaprofile.jpeg';
