50ec38ccf9cd283fcb9113c1abc8bc3b2d8a4df9
jithusonic <jithusonic33@gmail.com>
2026-05-13 22:52:02 +0530

    refactor: replace process route with clinical partnership anchor link in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export const Navbar = () => {
                     <ul className={styles.navLinks}>
                         <li><NavLink to="/about" onClick={() => setIsMenuOpen(false)} className={styles.navLink}>ABOUT</NavLink></li>
                         <li><NavLink to="/programs" onClick={() => setIsMenuOpen(false)} className={styles.navLink}>PROGRAMS</NavLink></li>
-                        <li><NavLink to="/process" onClick={() => setIsMenuOpen(false)} className={styles.navLink}>PROCESS</NavLink></li>
+                        <li><a href="/#clinical-partnership" onClick={() => setIsMenuOpen(false)} className={styles.navLink}>CLINICAL PARTNERSHIP</a></li>
                         <li><a href="/#testimonials" onClick={() => setIsMenuOpen(false)} className={styles.navLink}>TESTIMONIALS</a></li>
                     </ul>
 
