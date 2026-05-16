bed572b72ff020db5e3b645b97d405eab13627df
jithusonic <jithusonic33@gmail.com>
2026-05-16 12:41:20 +0530

    refactor: update professional title from Nutrition & Lifestyle Strategist to Nutrition & Wellness Advisor in AboutSection

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -14,7 +14,7 @@ export const AboutSection = () => {
                                 <Award size={20} color="#b59c73" />
                             </div>
                             <div className={styles.badgeText}>
-                                <strong>Global Nutrition & Lifestyle Strategist</strong>
+                                <strong>Global Nutrition & Wellness Advisor</strong>
                                 <span>Internationally Accredited</span>
                             </div>
                         </div>
